d65ee77f3495d574b8cbbbe36832944cd6db5532
dhoule <caine2003@gmail.com>
2019-08-09 07:40:04 -0500

    Updates spiders/fiveringsarmory.py
    #3 The spider seems to be staying within the given domain(s) now.

--- priceFinding/spiders/fiveringsarmory.py
+++ priceFinding/spiders/fiveringsarmory.py
@@ -4,13 +4,23 @@ import scrapy
 
 class FiveringsarmorySpider(scrapy.Spider):
   name = 'fiveringsarmory'
-  start_urls = ['http://fiveringsarmory.com/']
+  start_urls = ['http://fiveringsarmory.com/','https://fiveringsarmory.com/']
   handle_httpstatus_list = [404]
 
   def parse(self, response):
     yield { 
+      'url': response.url,
       'title': response.css('title').get()
     }
 
     for a in response.css('a'):
-      yield response.follow(a, callback=self.parse)
+      if "fiveringsarmory" in response.url:
+        yield response.follow(a, callback=self.parse)
+      else:
+        yield response.follow(a, callback=self.parseLeaf)
+
+  def parseLeaf(self, response):
+    yield { 
+      'url': response.url,
+      'title': response.css('title').get()
+    }
