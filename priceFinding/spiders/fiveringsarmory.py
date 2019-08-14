4c04d0749ca7bd711b8f4a1d26e835b8a0663654
dhoule <caine2003@gmail.com>
2019-08-14 12:13:05 -0500

    Updates spiders/fiveringsarmory.py
    #5 The spider is now able to log into a website.

--- priceFinding/spiders/fiveringsarmory.py
+++ priceFinding/spiders/fiveringsarmory.py
@@ -3,18 +3,35 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.linkextractors import IGNORED_EXTENSIONS
-import re
+import re # for Regex
+import os # for environment variable
 
 class FiveringsarmorySpider(scrapy.Spider):
   name = 'fiveringsarmory'
   allowed_domains = ['fiveringsarmory.com']
-  start_urls = ['http://fiveringsarmory.com/','https://fiveringsarmory.com/']
+  # start_urls = ['http://fiveringsarmory.com/','https://fiveringsarmory.com/']
+  start_urls = ['https://fiveringsarmory.com/login']
   handle_httpstatus_list = [404]
   rules = [
     Rule(LinkExtractor(deny=['\.pdf','\.jpg',]),follow=False)
   ]
 
   def parse(self, response):
+    return scrapy.FormRequest.from_response(
+      response, 
+      formdata={'email': os.environ['FRAUSERNAME'], 'password': os.environ['FRAPASSWORD']},
+      callback=self.after_login
+    )
+
+  def after_login(self, response):
+    if "Error while logging in" in response.body:
+      self.logger.error("Login failed!")
+      return
+    
+    # This is to redirect to the front page after a successful login
+    return scrapy.Request(url="https://fiveringsarmory.com/", callback=self.doTheThing)
+
+  def doTheThing(self, response):
     url = response.url
     if (re.search("\.jpg",url) == None) and (re.search("\.pdf",url) == None):
       yield { 
@@ -27,6 +44,6 @@ class FiveringsarmorySpider(scrapy.Spider):
       temp = a.attrib['href']
       # Only follow "fiveringsarmory" links to actual websites
       if ("fiveringsarmory" in temp) and (re.search('\.jpg',temp) == None) and (re.search('\.pdf',temp) == None):
-        yield response.follow(a, callback=self.parse)
+        yield response.follow(a, callback=self.doTheThing)
 
 
